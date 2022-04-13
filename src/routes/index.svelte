92cd2f36c3cfced9e95fe68bf6b266dd9142454a
GeneralMine <marvin@raiser.dev>
2022-04-13 20:49:11 +0200

    add lots of meta descriptions

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,10 @@
 <svelte:head>
-	<title>CampusCneipe</title>
+	<title>Die Campus Cneipe</title>
+	<meta property="og:title" content="Die Campus Cneipe" />
+	<meta property="og:url" content="https://c2.raiser.dev/" />
 	<meta name="description" content="Future website of the Campus Cneipe @ Technical University of Munich" />
+	<meta property="og:description" content="Future website of the Campus Cneipe @ Technical University of Munich" />
+	<meta property="og:image" content="https://c2.raiser.dev/favicon.png" />
 </svelte:head>
 
 <h1>Redesign of TUM Campus Cneipe</h1>
