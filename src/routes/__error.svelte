c6052381ba316e53e76b2636b1b787b4c8d029ba
GeneralMine <marvin@raiser.dev>
2022-04-25 23:03:55 +0200

    0.0.8

---
+++ src/routes/__error.svelte
@@ -0,0 +1,10 @@
+<script context="module">
+	export async function load({ url }) {
+		return {
+			status: 301,
+			redirect: 'https://www.c2.tum.de'
+		};
+	}
+</script>
+
+<slot />
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,22 @@
+<!-- <script context="module">
+	export async function load({ url }) {
+		if (url.protocol === 'https:') {
+			return {};
+		}
+		if (url.protocol === 'http:' && !url.hostname.startsWith('localhost')) {
+			return {
+				status: 301,
+				redirect: 'https://www.c2.tum.de'
+			};
+		}
+		return {};
+	}
+</script> -->
 <script>
 	import 'chota';
 	import '../app.css';
 	import Footer from '$lib/Layout/Footer.svelte';
 	import Nav from '$lib/Layout/Nav.svelte';
-	import { Container } from 'svelte-chota';
 </script>
 
 <div>
