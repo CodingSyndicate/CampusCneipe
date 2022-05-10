2b3eea62d4a7a1c4bf69dc9c4aaa5d0597aec90d
Marvin Raiser <marvin@raiser.dev>
2022-05-10 23:02:16 +0200

    0.1.5

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,17 +1,3 @@
-<!-- <script context="module">
-	export async function load({ url }) {
-		if (url.protocol === 'https:') {
-			return {};
-		}
-		if (url.protocol === 'http:' && !url.hostname.startsWith('localhost')) {
-			return {
-				status: 301,
-				redirect: 'https://www.c2.tum.de'
-			};
-		}
-		return {};
-	}
-</script> -->
 <script>
 	import 'chota';
 	import '../app.css';
