7640792de18d4d3ea0363a352ced2013038c0bfc
Marvin Raiser <marvin@raiser.dev>
2022-06-30 18:03:39 +0200

    intermediate push

--- src/lib/Reservierung/Reservierungstool.svelte
+++ src/lib/Reservierung/Reservierungstool.svelte
@@ -26,8 +26,6 @@
 			.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 	};
 
-	console.log(validateEmail('marvin@web.de'));
-
 	$: submitDisabled =
 		reservationDate === null ||
 		reservationTime === null ||
