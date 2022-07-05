d6df0d2beca336f0436a37cd280a13038ce7e947
GeneralMine <marvin@raiser.dev>
2022-07-05 10:31:55 +0200

    0.2.7

--- src/lib/Layout/Footer.svelte
+++ src/lib/Layout/Footer.svelte
@@ -9,7 +9,7 @@
 		<div class="impressum">
 			<a sveltekit:prefetch href="/impressum">Impressum</a>
 			&nbsp;
-			<a href="https://github.com/CodingSyndicate/CampusCneipe"> 0.2.6 </a>
+			<a href="https://github.com/CodingSyndicate/CampusCneipe"> 0.2.7 </a>
 			&nbsp;
 			<a sveltekit:prefetch href="/datenschutz">Datenschutz</a>
 		</div>
