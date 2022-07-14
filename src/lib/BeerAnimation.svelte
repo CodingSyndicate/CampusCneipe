acea592a1ad4706c90bf4a03c11de63b7a7f9581
GeneralMine <marvin@raiser.dev>
2022-07-14 20:02:30 +0200

    fix beer animation

---
+++ src/lib/BeerAnimation.svelte
@@ -0,0 +1,346 @@
+<script>
+	export let mirror = false;
+</script>
+
+<div class:mirror class="mainWrapper beerBG">
+	<div class="mainWrapperInner">
+		<div class="beer-wrapper">
+			<svg
+				version="1.1"
+				id="Layer_1"
+				xmlns="http://www.w3.org/2000/svg"
+				xlink="http://www.w3.org/1999/xlink"
+				x="0px"
+				y="0px"
+				viewBox="0 0 150 150"
+				enable-background="new 0 0 150 150"
+				xml:space="preserve"
+			>
+				<rect x="-1" y="-1" fill="#FFD18D" fill-opacity="0.0" width="153" height="153" />
+				<g id="beer-foam">
+					<path
+						fill="#FFE4C0"
+						stroke="#000000"
+						stroke-miterlimit="10"
+						d="M95.235,47.281c0-3.059-2.48-5.539-5.539-5.539
+          c-0.194,0-0.385,0.01-0.574,0.03c-0.01-4.396-3.575-7.956-7.973-7.956c-1.613,0-3.112,0.483-4.367,1.307
+          c-0.985-1.531-2.701-2.614-4.657-2.614c-2.166,0-4.036,0.991-4.947,2.991c-0.007,0-0.013,0-0.019,0
+          c-0.59,0-1.162,0.141-1.712,0.271c-1.009-1.065-2.434-1.701-4.018-1.701c-0.241,0-0.477,0.037-0.711,0.067
+          c-1.168-0.795-2.577-1.252-4.096-1.252c-3.276,0-6.047,2.166-6.966,5.14c-2.543,0.501-4.462,2.744-4.462,5.434
+          c0,3.059,2.48,5.54,5.539,5.54c0.97,0,1.881-0.25,2.674-0.689L95.235,47.281z"
+					/>
+					<circle id="foam-bubble_16_" fill="#FFFFFF" stroke="#000000" stroke-width="0.5" stroke-miterlimit="10" cx="54.04" cy="40.04" r="1.704" />
+					<circle id="foam-bubble_15_" fill="#FFFFFF" stroke="#000000" stroke-width="0.5" stroke-miterlimit="10" cx="69.871" cy="43.558" r="1.704" />
+					<circle id="foam-bubble_14_" fill="#FFFFFF" stroke="#000000" stroke-width="0.5" stroke-miterlimit="10" cx="83.944" cy="37.401" r="1.704" />
+					<circle id="foam-bubble_13_" fill="#FFFFFF" stroke="#000000" stroke-width="0.5" stroke-miterlimit="10" cx="57.393" cy="37.236" r="0.989" />
+					<circle id="foam-bubble_12_" fill="#FFFFFF" stroke="#000000" stroke-width="0.5" stroke-miterlimit="10" cx="66.188" cy="38.995" r="0.989" />
+					<circle id="foam-bubble_11_" fill="#FFFFFF" stroke="#000000" stroke-width="0.5" stroke-miterlimit="10" cx="80.261" cy="38.995" r="0.989" />
+					<circle id="foam-bubble_10_" fill="#FFFFFF" stroke="#000000" stroke-width="0.5" stroke-miterlimit="10" cx="86.417" cy="44.273" r="0.989" />
+					<circle id="foam-bubble_9_" fill="#FFFFFF" stroke="#000000" stroke-width="0.5" stroke-miterlimit="10" cx="58.566" cy="41.048" r="0.696" />
+					<circle id="foam-bubble_8_" fill="#FFFFFF" stroke="#000000" stroke-width="0.5" stroke-miterlimit="10" cx="53.289" cy="44.566" r="0.696" />
+					<circle id="foam-bubble_7_" fill="#FFFFFF" stroke="#000000" stroke-width="0.5" stroke-miterlimit="10" cx="49.771" cy="42.807" r="0.696" />
+					<circle id="foam-bubble_6_" fill="#FFFFFF" stroke="#000000" stroke-width="0.5" stroke-miterlimit="10" cx="65.602" cy="43.686" r="0.696" />
+					<circle id="foam-bubble_5_" fill="#FFFFFF" stroke="#000000" stroke-width="0.5" stroke-miterlimit="10" cx="83.192" cy="41.048" r="0.696" />
+					<circle id="foam-bubble_4_" fill="#FFFFFF" stroke="#000000" stroke-width="0.5" stroke-miterlimit="10" cx="90.228" cy="43.686" r="0.696" />
+					<circle id="foam-bubble_3_" fill="#FFFFFF" stroke="#000000" stroke-width="0.5" stroke-miterlimit="10" cx="85.831" cy="40.168" r="0.696" />
+					<circle id="foam-bubble_2_" fill="#FFFFFF" stroke="#000000" stroke-width="0.5" stroke-miterlimit="10" cx="79.674" cy="43.686" r="0.696" />
+					<circle id="foam-bubble_1_" fill="#FFFFFF" stroke="#000000" stroke-width="0.5" stroke-miterlimit="10" cx="70" cy="37.53" r="0.696" />
+					<circle id="foam-bubble" fill="#FFFFFF" stroke="#000000" stroke-width="0.5" stroke-miterlimit="10" cx="69.12" cy="40.168" r="0.696" />
+				</g>
+				<path id="beer-liquid" fill-opacity="1.0" fill="#E88F00" d="M96,114c0,0-9,5-26.5,5C54,119,43,114,43,114V47h53V114z" />
+				<path fill="#000000" fill-opacity="1.0" d="M99,152H41v-41c0,0,6.5,9,29,9c22,0,29-9,29-9V152z" />
+				<g>
+					<circle id="liquid-bubble_13_" fill="#FFFFFF" stroke="#000000" stroke-width="0.4" stroke-miterlimit="10" cx="64.492" cy="115.336" r="1.497" />
+					<circle id="liquid-bubble_12_" fill="#FFFFFF" stroke="#000000" stroke-width="0.4" stroke-miterlimit="10" cx="89.492" cy="110.336" r="1.497" />
+					<circle id="liquid-bubble_11_" fill="#FFFFFF" stroke="#000000" stroke-width="0.4" stroke-miterlimit="10" cx="49.492" cy="110.336" r="1.497" />
+					<circle id="liquid-bubble_10_" fill="#FFFFFF" stroke="#000000" stroke-width="0.4" stroke-miterlimit="10" cx="68.675" cy="116.153" r="0.681" />
+					<circle id="liquid-bubble_9_" fill="#FFFFFF" stroke="#000000" stroke-width="0.4" stroke-miterlimit="10" cx="75.675" cy="116.153" r="0.681" />
+					<circle id="liquid-bubble_8_" fill="#FFFFFF" stroke="#000000" stroke-width="0.4" stroke-miterlimit="10" cx="91.675" cy="112.153" r="0.681" />
+					<circle id="liquid-bubble_7_" fill="#FFFFFF" stroke="#000000" stroke-width="0.4" stroke-miterlimit="10" cx="46.675" cy="111.153" r="0.681" />
+					<circle id="liquid-bubble_6_" fill="#FFFFFF" stroke="#000000" stroke-width="0.4" stroke-miterlimit="10" cx="78.675" cy="116.153" r="0.681" />
+					<circle id="liquid-bubble_5_" fill="#FFFFFF" stroke="#000000" stroke-width="0.4" stroke-miterlimit="10" cx="67.874" cy="113.352" r="0.482" />
+					<circle id="liquid-bubble_4_" fill="#FFFFFF" stroke="#000000" stroke-width="0.4" stroke-miterlimit="10" cx="76.874" cy="114.352" r="0.482" />
+					<circle id="liquid-bubble_3_" fill="#FFFFFF" stroke="#000000" stroke-width="0.4" stroke-miterlimit="10" cx="92.874" cy="108.352" r="0.482" />
+					<circle id="liquid-bubble_2_" fill="#FFFFFF" stroke="#000000" stroke-width="0.4" stroke-miterlimit="10" cx="80.874" cy="116.352" r="0.482" />
+					<circle id="liquid-bubble_1_" fill="#FFFFFF" stroke="#000000" stroke-width="0.4" stroke-miterlimit="10" cx="52.874" cy="113.352" r="0.482" />
+					<circle id="liquid-bubble" fill="#FFFFFF" stroke="#000000" stroke-width="0.4" stroke-miterlimit="10" cx="57.874" cy="115.352" r="0.482" />
+				</g>
+				<g id="jug">
+					<path
+						fill="#FFFFFF"
+						d="M70.5,72c-0.828,0-1.5,0.57-1.5,1.272v33.074c0,0.703,0.672,1.272,1.5,1.272s1.5-0.57,1.5-1.272V73.272
+          C72,72.57,71.328,72,70.5,72z"
+					/>
+					<path
+						fill="#FFFFFF"
+						d="M55.5,72c-0.828,0-1.5,0.57-1.5,1.272v33.074c0,0.703,0.672,1.272,1.5,1.272s1.5-0.57,1.5-1.272V73.272
+          C57,72.57,56.328,72,55.5,72z"
+					/>
+					<path
+						fill="#FFFFFF"
+						d="M85.5,72c-0.828,0-1.5,0.57-1.5,1.272v33.074c0,0.703,0.672,1.272,1.5,1.272s1.5-0.57,1.5-1.272V73.272
+          C87,72.57,86.328,72,85.5,72z"
+					/>
+					<path
+						fill="#FFFFFF"
+						d="M99,57V45H41v66c0,1.126,0.813,11,29,11s29-9.874,29-11v-9.312c8.362-0.586,13-8.402,13-15.989V57H99z
+           M95,49v7H45v-7H95z M70,118c-19.882,0-24.79-5.124-25-7V59h50v52C95,111.07,93.531,118,70,118z M108,85.699
+          c0,5.585-3.561,11.346-9,11.983V61h9V85.699z"
+					/>
+				</g>
+			</svg>
+		</div>
+		<!-- -->
+	</div>
+</div>
+
+<style>
+	/* demo css (can be removed if seems not required)*/
+	* {
+		margin: 0;
+		padding: 0;
+	}
+	.mirror {
+		-moz-transform: scale(-1, 1);
+		-webkit-transform: scale(-1, 1);
+		-o-transform: scale(-1, 1);
+		-ms-transform: scale(-1, 1);
+		transform: scale(-1, 1);
+	}
+	.mainWrapper {
+		display: table;
+	}
+	.mainWrapperInner {
+		display: table-cell;
+		vertical-align: middle;
+	}
+
+	/**/
+
+	.beerBG {
+		background: transparent;
+	}
+	.beer-wrapper {
+		width: 300px;
+		margin: 0 auto;
+		background: transparent;
+	}
+	.beer-wrapper svg {
+		width: 100%;
+	}
+	#beer-liquid,
+	#beer-foam {
+		-webkit-animation: beerliquidupdown 8s ease-in-out infinite;
+		animation: beerliquidupdown 8s ease-in-out infinite;
+	}
+	circle[id^='foam-bubble'] {
+		-webkit-transform-origin: center;
+		transform-origin: center;
+		-webkit-animation: foambubble 2s ease-in-out infinite;
+		animation: foambubble 2s ease-in-out infinite;
+	}
+	circle[id^='foam-bubble']:nth-child(2),
+	circle[id^='foam-bubble']:nth-child(5),
+	circle[id^='foam-bubble']:nth-child(8),
+	circle[id^='foam-bubble']:nth-child(11),
+	circle[id^='foam-bubble']:nth-child(14),
+	circle[id^='foam-bubble']:nth-child(17) {
+		-webkit-animation-delay: 0.33s;
+		animation-delay: 0.33s;
+	}
+	circle[id^='foam-bubble']:nth-child(3),
+	circle[id^='foam-bubble']:nth-child(6),
+	circle[id^='foam-bubble']:nth-child(9),
+	circle[id^='foam-bubble']:nth-child(12),
+	circle[id^='foam-bubble']:nth-child(15),
+	circle[id^='foam-bubble']:nth-child(18) {
+		-webkit-animation-delay: 0.66s;
+		animation-delay: 0.66s;
+	}
+	circle[id^='foam-bubble']:nth-child(4),
+	circle[id^='foam-bubble']:nth-child(7),
+	circle[id^='foam-bubble']:nth-child(10),
+	circle[id^='foam-bubble']:nth-child(13),
+	circle[id^='foam-bubble']:nth-child(16),
+	circle[id^='foam-bubble']:nth-child(4) {
+		-webkit-animation-delay: 1;
+		animation-delay: 1;
+	}
+	circle[id^='liquid-bubble'] {
+		-webkit-animation: liquidbubble 1s ease-in infinite;
+		animation: liquidbubble 1s ease-in infinite;
+	}
+	circle[id^='liquid-bubble']:nth-child(1),
+	circle[id^='liquid-bubble']:nth-child(4),
+	circle[id^='liquid-bubble']:nth-child(7),
+	circle[id^='liquid-bubble']:nth-child(10),
+	circle[id^='liquid-bubble']:nth-child(13) {
+		-webkit-animation-delay: 0.33s;
+		animation-delay: 0.33s;
+	}
+	circle[id^='liquid-bubble']:nth-child(2),
+	circle[id^='liquid-bubble']:nth-child(5),
+	circle[id^='liquid-bubble']:nth-child(8),
+	circle[id^='liquid-bubble']:nth-child(11),
+	circle[id^='liquid-bubble']:nth-child(14) {
+		-webkit-animation-delay: 0.66s;
+		animation-delay: 0.66s;
+	}
+	circle[id^='liquid-bubble']:nth-child(3),
+	circle[id^='liquid-bubble']:nth-child(6),
+	circle[id^='liquid-bubble']:nth-child(9),
+	circle[id^='liquid-bubble']:nth-child(12) {
+		-webkit-animation-delay: 1;
+		animation-delay: 1;
+	}
+	@-webkit-keyframes beerliquidupdown {
+		0% {
+			-webkit-transform: translateY(65px);
+			transform: translateY(65px);
+		}
+		10% {
+			-webkit-transform: translateY(0px);
+			transform: translateY(0px);
+		}
+		30% {
+			-webkit-transform: translateY(0px);
+			transform: translateY(0px);
+		}
+		40% {
+			-webkit-transform: translateY(0px);
+			transform: translateY(0px);
+		}
+		50% {
+			-webkit-transform: translateY(22px);
+			transform: translateY(22px);
+		}
+		60% {
+			-webkit-transform: translateY(22px);
+			transform: translateY(22px);
+		}
+		70% {
+			-webkit-transform: translateY(44px);
+			transform: translateY(44px);
+		}
+		80% {
+			-webkit-transform: translateY(44px);
+			transform: translateY(44px);
+		}
+		90% {
+			-webkit-transform: translateY(65px);
+			transform: translateY(65px);
+		}
+		100% {
+			-webkit-transform: translateY(65px);
+			transform: translateY(65px);
+		}
+	}
+	@keyframes beerliquidupdown {
+		0% {
+			-webkit-transform: translateY(65px);
+			transform: translateY(65px);
+		}
+		10% {
+			-webkit-transform: translateY(0px);
+			transform: translateY(0px);
+		}
+		30% {
+			-webkit-transform: translateY(0px);
+			transform: translateY(0px);
+		}
+		40% {
+			-webkit-transform: translateY(0px);
+			transform: translateY(0px);
+		}
+		50% {
+			-webkit-transform: translateY(22px);
+			transform: translateY(22px);
+		}
+		60% {
+			-webkit-transform: translateY(22px);
+			transform: translateY(22px);
+		}
+		70% {
+			-webkit-transform: translateY(44px);
+			transform: translateY(44px);
+		}
+		80% {
+			-webkit-transform: translateY(44px);
+			transform: translateY(44px);
+		}
+		90% {
+			-webkit-transform: translateY(65px);
+			transform: translateY(65px);
+		}
+		100% {
+			-webkit-transform: translateY(65px);
+			transform: translateY(65px);
+		}
+	}
+	@-webkit-keyframes foambubble {
+		0% {
+			opacity: 0;
+			-webkit-transform: scale(0);
+			transform: scale(0);
+		}
+		50% {
+			opacity: 1;
+			-webkit-transform: scale(1);
+			transform: scale(1);
+		}
+		100% {
+			opacity: 0;
+			-webkit-transform: scale(0);
+			transform: scale(0);
+		}
+	}
+	@keyframes foambubble {
+		0% {
+			opacity: 0;
+			-webkit-transform: scale(0);
+			transform: scale(0);
+		}
+		50% {
+			opacity: 1;
+			-webkit-transform: scale(1);
+			transform: scale(1);
+		}
+		100% {
+			opacity: 0;
+			-webkit-transform: scale(0);
+			transform: scale(0);
+		}
+	}
+	@-webkit-keyframes liquidbubble {
+		0% {
+			opacity: 1;
+			-webkit-transform: scale(1);
+			transform: scale(1);
+			-webkit-transform: translateY(0);
+			transform: translateY(0);
+		}
+		100% {
+			opacity: 0;
+			-webkit-transform: scale(0);
+			transform: scale(0);
+			-webkit-transform: translateY(-30px);
+			transform: translateY(-30px);
+		}
+	}
+	@keyframes liquidbubble {
+		0% {
+			opacity: 1;
+			-webkit-transform: scale(1);
+			transform: scale(1);
+			-webkit-transform: translateY(0);
+			transform: translateY(0);
+		}
+		100% {
+			opacity: 0;
+			-webkit-transform: scale(0);
+			transform: scale(0);
+			-webkit-transform: translateY(-30px);
+			transform: translateY(-30px);
+		}
+	}
+</style>
