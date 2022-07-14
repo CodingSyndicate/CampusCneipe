949c7e6a51d5786b796e5361c178c484de7cbafd
GeneralMine <marvin@raiser.dev>
2022-07-14 20:32:03 +0200

    fix anfahrt embed size

--- src/routes/anfahrt.svelte
+++ src/routes/anfahrt.svelte
@@ -3,7 +3,11 @@
 
 	import { Container, Tag, Button } from 'svelte-chota';
 	let googleMapsAccepted = false;
+	let innerWidth;
+	let innerHeight;
 </script>
+
+<svelte:window bind:innerWidth bind:innerHeight />
 
 <svelte:head>
 	<title>Anfahrt | CampusCneipe</title>
@@ -19,8 +23,8 @@
 				<iframe
 					title="Anfahrt zur Campus Cneipe über Google Maps"
 					src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d1610.3112722950407!2d11.670865797218344!3d48.26652079580697!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e3!4m5!1s0x479e72ece78d321f%3A0xf8d2874f0eb7c24c!2sGarching-Forschungszentrum%2C%20Garching%20bei%20M%C3%BCnchen!3m2!1d48.265043299999995!2d11.6715693!4m5!1s0x479e7292ea3852a1%3A0xab73615c78d2ce12!2sCampus-Cneipe%20C2%2C%20Boltzmannstra%C3%9Fe%2019%2C%2085748%20Garching%20bei%20M%C3%BCnchen!3m2!1d48.2678406!2d11.6722871!5e0!3m2!1sde!2sde!4v1657822733861!5m2!1sde!2sde"
-					width="800"
-					height="600"
+					width={innerWidth > 800 ? 800 : innerWidth}
+					height={innerHeight > 600 ? 600 : innerHeight}
 					style="border:0;"
 					allowfullscreen=""
 					loading="lazy"
