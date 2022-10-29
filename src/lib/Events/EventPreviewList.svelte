85061070ac48e9c6f9b279ad44e2e06cbc0a7603
Kilian Holzinger <kilian.holzinger@gmail.com>
2022-10-29 15:15:30 +0200

    WIP: Better event display

--- src/lib/Events/EventPreviewList.svelte
+++ src/lib/Events/EventPreviewList.svelte
@@ -1,12 +1,36 @@
-<script>
-	import EventPreview from './EventPreview.svelte';
-	export let events;
+<script>  
+  import EventPreview from './EventPreview.svelte';
+  export let events;
+
+  events.forEach(event => {
+    if(Array.isArray(event.date)) {
+      event.date.forEach(date => {
+	var new_event = event;
+	new_event.date = date;
+	events.push(new_event);
+      })
+    }
+  });
+  for (let event in events) {
+    if (typeof events[event].date == "string") {
+      events[event].date = Date.parse(events[event].date);
+    }
+  }
+  events.sort((a, b) => {
+    if (a.date < b.date) return -1;
+    else return 1;
+  });
+  let day_milliseconds = 1000 * 60 * 60 * 24;
+  let cutoff = new Date().getTime() - day_milliseconds;
 </script>
 
 <div class="rowContainer">
-	{#each events as event}
-		<EventPreview {event} />
-	{/each}
+  {#each events as event}
+    {#if event.date - cutoff >= 0}
+      {@debug event}
+    <EventPreview {event} />
+  {/if}
+  {/each}
 </div>
 
 <style>
