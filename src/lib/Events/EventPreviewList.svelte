22a206be691c89115e2f4ef76dde37ac899703ba
Marvin Raiser <marvin@raiser.dev>
2022-11-11 21:32:58 +0100

    formatting and data types

--- src/lib/Events/EventPreviewList.svelte
+++ src/lib/Events/EventPreviewList.svelte
@@ -1,25 +1,24 @@
 <script>
-  import events from '$data/event_data.json';
-  import EventPreview from './EventPreview.svelte';
+	import events from '$data/event_data.json';
+	import EventPreview from './EventPreview.svelte';
 
-  export let count = 7;
-  for (let event in events) {
-    if (typeof events[event].begin == "string") {
-      events[event].begin = Date.parse(events[event].begin);
-    }
-  }
+	const count = 7;
+	for (let event in events) {
+		if (typeof events[event].begin == 'string') {
+			events[event].begin = Date.parse(events[event].begin);
+		}
+	}
 
-  events.sort((a, b) => {
-    if (a.begin < b.begin) return -1;
-    else return 1;
-  });
-  let day_milliseconds = 1000 * 60 * 60 * 24;
-  let cutoff = new Date().getTime() - day_milliseconds;
+	events.sort((a, b) => {
+		if (a.begin < b.begin) return -1;
+		else return 1;
+	});
+	const day_milliseconds = 1000 * 60 * 60 * 24;
+	let cutoff = new Date().getTime() - day_milliseconds;
 </script>
 
 {#each events as event}
-  {#if event.begin - cutoff >= 0}
-    <EventPreview {event} />
-  {/if}
+	{#if event.begin - cutoff >= 0}
+		<EventPreview {event} />
+	{/if}
 {/each}
-
