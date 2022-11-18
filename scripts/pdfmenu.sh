fb0f866b028ebd8739b3ed0f0cd100a6b924bad6
Kilian Holzinger <kilian.holzinger@gmail.com>
2022-11-19 00:28:15 +0100

    Trying lualatex

--- scripts/pdfmenu.sh
+++ scripts/pdfmenu.sh
@@ -4,4 +4,4 @@ set -eu
 cd "$(dirname "${BASH_SOURCE[0]}")"
 sed 's/\&/+/g' ../data/menu_data.json > ./menu_data.json.tmp
 chevron -d ./menu_data.json.tmp ./menu.tex.mustache > ./menu.tex
-pdflatex menu.tex
+lualatex menu.tex
