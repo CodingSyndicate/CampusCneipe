645954a1cdda3a68f864256a79f7887d5a16b152
Kilian Holzinger <kilian.holzinger@gmail.com>
2022-12-22 21:29:24 +0100

    Add Betriebsferien feature

--- src/routes/Oeffnungszeiten.svelte
+++ src/routes/Oeffnungszeiten.svelte
@@ -1,8 +1,34 @@
 <script>
-	import opening_times from '$data/opening_times.json';
-	import { goto } from '$app/navigation';
-	import Card from '$lib/Cards/Card.svelte';
-	let bigImage = '/images/7.jpg';
+  import opening_times from '$data/opening_times.json';
+  import { goto } from '$app/navigation';
+  import Card from '$lib/Cards/Card.svelte';
+  let bigImage = '/images/7.jpg';
+
+  let options = {
+//    weekday: 'long',
+    year: '2-digit',
+    month: 'numeric',
+    day: 'numeric',
+//    hour: 'numeric',
+//    minute: 'numeric',
+    timeZone: 'Europe/Berlin'
+  };
+  
+  for (let vacation in opening_times.vacations) {
+    opening_times.vacations[vacation].begin_date = Date.parse(opening_times.vacations[vacation].from)
+    opening_times.vacations[vacation].begin_text = new Intl.DateTimeFormat('de-DE', options).format(opening_times.vacations[vacation].begin_date)
+    opening_times.vacations[vacation].end_date = Date.parse(opening_times.vacations[vacation].to)
+    opening_times.vacations[vacation].end_text = new Intl.DateTimeFormat('de-DE', options).format(opening_times.vacations[vacation].end_date)
+
+  }
+  let day_milliseconds = 1000 * 60 * 60 * 24;
+  let cutoff = new Date().getTime() + 20 * day_milliseconds;
+
+  opening_times.vacations.sort((a, b) => {
+    if (a.begin_date < b.begin_date) return -1;
+    else return 1;
+  });
+  let display_vacation = opening_times.vacations.filter(v => v.begin_date - cutoff < 0)[0];
 </script>
 
 <Card
@@ -13,15 +39,29 @@
 	sideImageHalf={true}
 	small={true}
 	darkBackground={true}
->
-	<ul>
-		{#each opening_times as time}
-			<li>{time.day}: {time.open} - {time.close} Uhr</li>
-		{/each}
-	</ul>
-	<p>
+  >
+  {#if display_vacation}
+      <div class="alert alert-danger">
+	Von {display_vacation.begin_text} bis {display_vacation.end_text} wegen {display_vacation.name} geschlossen!
+      </div>
+    {/if}
+
+  <table class="table mt-3 table-borderless text-light">
+      <tbody>
+	{#each opening_times.days as day}
+	  <tr>
+	    <th>{day.day}</th>
+	    <th>{day.open}</th>
+	    <th>-</th>
+	    <th>{day.close}</th>
+	  </tr>
+	{/each}
+      </tbody>
+  </table>
+
+		<p>
 		Außerhalb der Vorlesungszeit haben wir für den regulären Betrieb geschlossen. Die Campus Cneipe
-		kann aber weiterhin für private Veranstaltungen jeglicher Art gebucht werden.
+		kann aber weiterhin für Veranstaltungen jeglicher Art gebucht werden.
 	</p>
 	<div class="buttons">
 		<button type="button" class="btn btn-primary" on:click={() => goto('/kontakt')}>
@@ -29,7 +69,7 @@
 		</button>
 		&nbsp;
 		<button type="button" class="btn btn-outline-primary" on:click={() => goto('/mieten')}>
-			C2 Mieten
+			C2 mieten
 		</button>
 	</div>
 </Card>
