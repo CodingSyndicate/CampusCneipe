85ab9e3a1da5b352d601b3baaf32aa7a625192cf
Kilian Holzinger <kilian.holzinger@gmail.com>
2023-02-03 12:52:02 +0100

    Add no events message

--- src/lib/Events/EventPreviewList.svelte
+++ src/lib/Events/EventPreviewList.svelte
@@ -2,11 +2,14 @@
   import EventPreview from '$lib/Events/EventPreview.svelte';
   export let display_events;
 </script>
-
-{#each display_events as event, i}
-  {#if i % 2 == 0}
-    <EventPreview {event} textRight={false} />
-  {:else}
-    <EventPreview {event} textRight={true} />
-  {/if}
-{/each}
+{#if display_events.length < 1}
+  <div class="text-center mt-5">keine bevorstehenden Events</div>
+{:else}
+  {#each display_events as event, i}
+    {#if i % 2 == 0}
+      <EventPreview {event} textRight={false} />
+    {:else}
+      <EventPreview {event} textRight={true} />
+    {/if}
+  {/each}
+{/if}
